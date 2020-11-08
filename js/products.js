e6b0a87cbcf1a69ec7cfd9409b0da921f01b483e
veero364 <veero364@users.noreply.github.com>
2020-11-08 17:49:50 -0300

    08112020
    Se crea Perfil.
    Responsive

--- js/products.js
+++ js/products.js
@@ -79,7 +79,7 @@ function showProductsList(currentCategoriesArray) { //MOSTRAR PRODUCTOS
 			<div class="col">
 			<div class="d-flex w-100 justify-content-between">
 			<h4 class="mb-1">`+ category.name +' ----> '+ category.currency+'  '+category.cost+`</h4>
-			<small class="text-muted">` + category.soldCount + ` artículos</small>
+			<small class="text-muted" id="divVen">` + category.soldCount + ` artículos</small>
 			
 			</div>
 			<div class="vero w-100 justify-content-between">
--- js/userName.js
+++ js/userName.js
@@ -2,13 +2,17 @@
 function validate()
 {
 var user = document.f1.email.value;//creo una variable que almacena el valor ingresado en el campo usuario. f1=name del form
+
+
 // Chequeo que soporte el browser
 if (typeof(Storage) !== "undefined") {
   // Si hay algo definido lo almaceno
   localStorage.setItem("usuario", user);
 	document.f1.action="ppal.html";
 	return true;
-} else {
+} 
+else 
+{
   alert("Sorry, your browser does not support Web Storage...");//si el browser no lo soporta envia esta alerta de error
   document.f1.action="ppal.html";
 	return true;
@@ -17,5 +21,28 @@ if (typeof(Storage) !== "undefined") {
 
   function myFunction()
    {
+  
 	   document.getElementById("user").innerHTML = localStorage.getItem("usuario");
-   }
+	   document.getElementById("user14").value = localStorage.getItem("usuario");
+
+	 document.getElementById("userPerfil_priNom").value = localStorage.getItem("priNom");
+	  document.getElementById("userPerfil_segNom").value = localStorage.getItem("segNom");
+	  document.getElementById("userPerfil_priAp").value = localStorage.getItem("priAp");
+	  document.getElementById("userPerfil_segAp").value = localStorage.getItem("segAp");
+	 document.getElementById("idEdad").value = localStorage.getItem("edad");
+	  document.getElementById("Telefono").value = localStorage.getItem("Tel");
+	 document.getElementById("idLink").value = localStorage.getItem("linklink");
+	 let h=localStorage.getItem("linklink");
+	document.getElementById("FotoInseted").innerHTML = h;
+	 
+  }
+
+
+
+
+
+
+
+
+
+
